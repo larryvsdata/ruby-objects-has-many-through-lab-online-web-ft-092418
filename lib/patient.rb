937bebb6adb5ae656677a001d6deffed28447723
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-06 13:31:07 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,23 @@
+class Patient
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+   @name = name
+   @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  
+   def new_appointment(patient, date)
+    appointment = Appointment.new(date, patient ,self)
+    @patients << patient
+    @appointments << appointment
+    appointment
+  end
+  
+end
